8035f63c769313414a27d9c3e93aa8309de203ed
sk-phan <sukiphan97@gmail.com>
2023-10-21 16:54:24 +0300

    added loader

--- code/src/components/MapComponent.tsx
+++ code/src/components/MapComponent.tsx
@@ -3,7 +3,11 @@ import { MapContainer, TileLayer, Marker, Popup  } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css';
 import "../styles/MapComponent.css";
 
-const MapComponent = () => {
+interface Props {
+    currentPosition: [number, number];
+}
+
+const MapComponent = ({ currentPosition } : Props) => {
     const position: LatLngExpression = [51.505, -0.09]
     const customIcon = new Icon({
         iconUrl: "https://unpkg.com/leaflet@1.5.1/dist/images/marker-icon.png",
@@ -14,15 +18,17 @@ const MapComponent = () => {
 
     return (
         <div className='map-container'>
-             <MapContainer  style={{ width: "100%", height: "100%" }}  center={position} zoom={13} scrollWheelZoom={false}>
+            <MapContainer  style={{ width: "100%", height: "100%" }}  center={currentPosition} zoom={13} scrollWheelZoom={false}>
             <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={position} icon={customIcon}>
+            <Marker position={currentPosition} icon={customIcon}>
             <Popup>
                 A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
+            </Marker>
+            <Marker position={[52.505, -0.09]} icon={customIcon}>  
             </Marker>
             </MapContainer>
         </div>
--- code/src/views/MapView.tsx
+++ code/src/views/MapView.tsx
@@ -1,15 +1,39 @@
 
+import { useEffect, useState } from 'react';
 import MapComponent from '../components/MapComponent';
 import SearchBar from '../components/SearchBar';
 import '../styles/MapView.css';
 
 const MapView = () => {
+
+    const [currentPosition, setCurrentPosition] = useState<[number, number]>([0,0]);
+    const [loading, setLoading] = useState(true)
    
+    useEffect(() => {
+        setLoading(true)
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                const latitude = position.coords.latitude;
+                const longitude = position.coords.longitude
+                setCurrentPosition([ latitude, longitude ])
+                setLoading(false)
+            });
+
+          } else {
+            alert("Geolocation is not supported by this browser.");
+            setLoading(false)
+          }
+    }, [])
 
     return (
         <div className="map-view-container">
             <SearchBar />
-            <MapComponent/>
+            {
+                loading ? <p>Loading</p>
+                :
+                <MapComponent currentPosition = {currentPosition}/>
+            }
         </div>
     
     )
