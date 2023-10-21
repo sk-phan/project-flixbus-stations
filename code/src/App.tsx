09a869e7aede87d4e5c228360471eca5b8281bba
sk-phan <sukiphan97@gmail.com>
2023-10-21 14:15:24 +0300

    added nav bar

--- code/src/App.tsx
+++ code/src/App.tsx
@@ -1,49 +1,12 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
 
-function App() {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('https://api.digitransit.fi/timetables/v1/hsl', {
-          method: 'GET',
-          headers: {
-            'Cache-Control': 'no-cache',
-            'Authorization': '8bb8bdaa0fff47d5a5a3a2702c50c596', // Replace with your actual access token if needed
-            'digitransit-subscription-key': '8bb8bdaa0fff47d5a5a3a2702c50c596', // Replace with your API key
-          },
-        });
+const App = () => {
 
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        const data = await response.json();
-        console.log(data);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
     </div>
   );
 }
---
+++ code/src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import "../styles/NavBar.css"
+
+const NavBar = () => {
+
+    return (
+        <nav>
+            <img className="nav-logo" src="/Flixbus-white.png" alt="flixbus" width="100" />
+        </nav>
+    )
+}
+
+export default NavBar;
