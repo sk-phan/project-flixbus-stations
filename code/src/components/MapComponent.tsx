42e301f0a1c705f51ef63cf43db8b26dcbb1b529
sk-phan <sukiphan97@gmail.com>
2023-10-22 15:24:16 +0300

    handle case when user denies location access

--- code/src/components/MapComponent.tsx
+++ code/src/components/MapComponent.tsx
@@ -1,8 +1,8 @@
-import { LatLngExpression, Icon, Map } from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup, Tooltip  } from 'react-leaflet'
+import { Icon, LatLngExpression } from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup, Tooltip, useMap } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import "../styles/MapComponent.css";
-import { RefObject, useEffect, useLayoutEffect, useRef,  } from 'react';
+import { useEffect } from 'react';
 import { Place } from '../types';
 import L from 'leaflet';
 
@@ -10,15 +10,12 @@ interface Props {
     currentPosition: [number, number];
     busStops: Place[];
     zoomedLocation: Place;
-    loading: boolean
+    loading: boolean;
 }
 
-const zoom = 13
+const zoom = 13;
 
-const MapComponent = ({ currentPosition, busStops, zoomedLocation, loading } : Props) => {
-
-    const mapRef: RefObject<Map> = useRef(null);
-
+const MapComponent = ({ currentPosition, busStops, zoomedLocation, loading }: Props) => {
     const customIcon = new Icon({
         iconUrl: "/marker.png",
         iconSize: [41, 41],
@@ -31,97 +28,98 @@ const MapComponent = ({ currentPosition, busStops, zoomedLocation, loading } : P
         iconSize: [41, 41],
         iconAnchor: [10, 41],
         popupAnchor: [2, -40]
-    })
+    });
 
     useEffect(() => {
-        const map = mapRef.current;
-        if (zoomedLocation.id !== "") {
-            if (map) {
-                const coords: LatLngExpression = [ zoomedLocation.location.lat, zoomedLocation.location.lon ]
+        console.log("Map Component Mounted");
+    }, []);
 
-                map.setView(coords, 30);
+    const MapComponentView = () => {
+        const map = useMap();
 
-                const bounds = L.latLngBounds([
+        useEffect(() => {
+            if (zoomedLocation.id !== "") {
+                const coords: LatLngExpression = [zoomedLocation.location.lat, zoomedLocation.location.lon];
+
+                map.setView(coords, 20);
+
+                const zoomedLocationBounds = L.latLngBounds([
                     [zoomedLocation.location.lat, zoomedLocation.location.lon],
-                    currentPosition
                 ]);
-        
-                map.fitBounds(bounds, { padding: [50, 50] });
+                const currentPositionBounds = L.latLngBounds([currentPosition]);
+
+                const combinedBounds = zoomedLocationBounds.extend(currentPositionBounds);
+
+                map.fitBounds(combinedBounds, { padding: [50, 50] });
+            } 
+            else {
+                setBounds(map);
             }
-        }
-        else {
-           setBounds()
-        }
-    }, [zoomedLocation, currentPosition])
-    
+        }, [zoomedLocation, busStops, currentPosition]);
 
-    const setBounds = () => {
-        const map = mapRef.current;
 
-        if (map) {
-
+        const setBounds = (mapInstance: L.Map) => {
             if (busStops.length > 0) {
                 const latitudes = busStops.map((bus) => bus.location.lat);
                 const longitudes = busStops.map((bus) => bus.location.lon);
-          
-                const minLat = Math.min(...latitudes);
-                const maxLat = Math.max(...latitudes);
-                const minLon = Math.min(...longitudes);
-                const maxLon = Math.max(...longitudes);
-    
+
+                const minLat = Math.min(...latitudes, currentPosition[0]);
+                const maxLat = Math.max(...latitudes, currentPosition[0]);
+                const minLon = Math.min(...longitudes, currentPosition[1]);
+                const maxLon = Math.max(...longitudes, currentPosition[1]);
+
                 const bound = L.latLngBounds([
                     [minLat, minLon],
                     [maxLat, maxLon],
-                  ]);
-    
-                map.fitBounds(bound, { padding: [50, 50] });
+                ]);
+                mapInstance.fitBounds(bound, { padding: [50, 50] });
             }
             else if (busStops.length === 0) {
 
                 if ( JSON.stringify(currentPosition) === "[0,0]") {
-                    console.log("hello")
                     map.fitBounds([[-90,180], [90,-180]], { padding: [50, 50] });
                 }
                 else {
                     const bound = L.latLngBounds([
                         currentPosition,
                       ]);
-    
-                      console.log(currentPosition)
-        
+            
                     map.fitBounds(bound, { padding: [50, 50] });
                     map.setView(currentPosition, 30);
                 }
             }
-        }
-    }
+        };
+
+        return null; 
+    };
 
     return (
         <div className='map-container'>
-            <MapContainer ref={mapRef} style={{ width: "100%", height: "100%" }}  zoom={zoom} scrollWheelZoom={true}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={currentPosition} icon={customIcon}>
-            <Tooltip direction="bottom" offset={[0, 0]} opacity={1} permanent>
-                You're here
-            </Tooltip>
-            </Marker>
-            { busStops.length > 0 && busStops.map(bus => (
-                <Marker key={bus.id} position={[bus.location.lat, bus.location.lon]} icon={customBusIcon}>
+            <MapContainer style={{ width: "100%", height: "100%" }} center={[60.1699, 24.9384]} zoom={zoom} scrollWheelZoom={true}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {JSON.stringify(currentPosition) !== "[0,0]" && <Marker position={currentPosition} icon={customIcon}>
                     <Tooltip direction="bottom" offset={[0, 0]} opacity={1} permanent>
-                        {bus.name}
+                        You're here
                     </Tooltip>
-                    <Popup>
-                        { bus.address }, { bus.zipcode } {bus.city.name}
-                    </Popup>
-                </Marker>
-            ))
-            }
+                </Marker>}
+                {busStops.length > 0 &&
+                    busStops.map((bus) => (
+                        <Marker key={bus.id} position={[bus.location.lat, bus.location.lon]} icon={customBusIcon}>
+                            <Tooltip direction="bottom" offset={[0, 0]} opacity={1} permanent>
+                                {bus.name}
+                            </Tooltip>
+                            <Popup>
+                                {bus.address}, {bus.zipcode} {bus.city.name}
+                            </Popup>
+                        </Marker>
+                    ))}
+                <MapComponentView />
             </MapContainer>
         </div>
-    )
-}
+    );
+};
 
 export default MapComponent;
